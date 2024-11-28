39b69400d6dfd5bceb11fc836729bca7d62d3b6a
Linda <linda.fitriani@live.com>
2024-11-28 19:06:07 +0700

    Update cleanup.ps1: Remove auth file deletion commands

--- cleanup.ps1
+++ cleanup.ps1
@@ -4,5 +4,3 @@ Remove-Item -Path .\package-lock.json -Force -ErrorAction SilentlyContinue
 npm install
 
 
-Remove-Item -Path "Z:\DEV\Puppeteer-projects\UPWK-Puppeteer\src\data\auth\cookies\auth_cookies.json" -Force
-Remove-Item -Path "Z:\DEV\Puppeteer-projects\UPWK-Puppeteer\src\data\auth\localStorage\auth_localStorage.json" -Force
